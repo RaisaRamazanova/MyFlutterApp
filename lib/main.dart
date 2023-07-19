da718045c50e318e89e4f920c67f351340885881
Раисат Рамазанова <raisatramazanova@MacBook-Air-Raisat.local>
2023-07-19 20:32:16 +0300

    worked with widgests

---
+++ lib/main.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  home: Home()
+));
+
+class Home extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('app'),
+        centerTitle: true,
+        backgroundColor: Colors.green,
+      ),
+      body: Row(
+        children: <Widget> [
+          Expanded(
+            flex: 3,
+              child: Image.asset('assets/cat.png')),
+          Expanded(
+            flex: 3,
+            child: Container(
+              child: Container(
+                padding: EdgeInsets.all(40.0),
+                color: Colors.amber,
+                child: Text('1'),
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 2,
+            child: Container(
+              padding: EdgeInsets.all(40.0),
+              color: Colors.red,
+              child: Text('2'),
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Container(
+              padding: EdgeInsets.all(40.0),
+              color: Colors.cyan,
+              child: Text('3'),
+            ),
+          )
+        ]
+      ),
+      floatingActionButton: FloatingActionButton(
+      onPressed: () {},
+    child: Text('Click'),
+    backgroundColor: Colors.amber[500],
+    ),
+  );
+  }
+}
